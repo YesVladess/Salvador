ea40735bf7fb6bd2315ce02619de08d2089d3603
Vlad Kononenko <vlad.kononenko95@gmail.com>
2023-11-01 10:54:45 +0600

    Initial Commit

---
+++ Salvador/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  Copyright (c) 2011-2022, Zingaya, Inc. All rights reserved.
+//
+
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
